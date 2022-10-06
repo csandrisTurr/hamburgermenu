451efa1937f03e33ee4da364be9b06656a052aee
csandrisTurr <csaszarandras@turr.hu>
2022-10-06 08:44:41 +0200

    befejezes

--- topbar.js
+++ topbar.js
@@ -1,8 +1,43 @@
+let topbar = null;
+let topbarIcon = null;
+let topbarItems = null;
+let hiddenBurger = true;
+
+const mediaQuery = matchMedia("(max-width: 768px)");
+mediaQuery.onchange = e => {
+    if (e.matches) {
+        // smaller
+        updateTopbarState();
+    } else {
+        // bigger
+        topbarItems.style.display = "flex";
+    }
+};
+
 function init_topbar() {
     const topbars = document.getElementsByClassName("top-bar");
     if (topbars.length > 1) throw new Error("ne legyen tobb topbar testverem kerlek");
 
-    const topbar = topbars[0];
+    topbarIcon = document.getElementsByClassName("top-bar__icon")[0];
+    topbar = topbars[0];
+    topbarItems = document.getElementsByClassName("top-bar__items")[0];
+
+    topbarIcon.addEventListener("click", click);
+
+    if (mediaQuery.matches) {
+        updateTopbarState();
+    }
+}
+
+function updateTopbarState() {
+    if (!!topbarItems) {
+        topbarItems.style.display = hiddenBurger ? "none" : "flex";
+    }
+}
+
+function click() {
+    hiddenBurger = !hiddenBurger;
+    updateTopbarState();
 }
 
 init_topbar()
